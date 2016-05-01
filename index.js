2035522722a7bbe99a908b8086c394666ab2b0ee
Julian Gruber <julian@juliangruber.com>
2016-05-01 11:49:52 +0200

    workey work

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 
+const versions = [
+  'FICHIER GUITAR PRO v5.00',
+  'FICHIER GUITAR PRO v5.10'
+];
+
 module.exports = buf => {
+  let version;
+  let versionIndex;
+
+  const readVersion = () => {
+    /*if (version === null)*/ version = readStringByte(30);
+  };
+
+  const isSupportedVersion = (version) => {
+    for (let i = 0; i < versions.length; i++) {
+      if (versions[i] === version) {
+        versionIndex = i;
+        return true;
+      }
+    }
+    return false;
+  };
+
   const readUnsignedByte = () => {
     const num = buf.readUInt8();
     buf = buf.slice(1);
@@ -14,9 +36,13 @@ module.exports = buf => {
 
   const readString = (size, len) => {
     if (typeof len == 'undefined') len = size;
-    const str = buf.toString('utf8', 0, len);
-    buf = buf.slice(size);
-    return str;
+    const bytes = buf.slice(0, size > 0
+      ? size
+      : len);
+    buf = buf.slice(bytes.length);
+    return bytes.toString('utf8', 0, len >= 0 && len <= bytes.length
+      ? len
+      : size);
   };
 
   const readStringByte = (size) => {
@@ -31,8 +57,14 @@ module.exports = buf => {
     return readString(readInt());
   };
 
-  const versionString = readStringByte(30);
-  const [, major, minor] = /v(\d+)\.(\d+)/.exec(versionString);
+  const skip = (n) => {
+    buf = buf.slice(n);
+  };
+
+  readVersion();
+  if (!isSupportedVersion(version)) throw new Error('unsupported version');
+
+  const [, major, minor] = /v(\d+)\.(\d+)/.exec(version);
 
   const title = readStringByteSizeOfInteger();
   const subtitle = readStringByteSizeOfInteger();
@@ -58,6 +90,17 @@ module.exports = buf => {
     readStringInteger();
   }
 
+  // page setup
+  skip(versionIndex > 0
+    ? 49
+    : 30);
+  for (let i = 0; i < 11; i++) {
+    skip(4);
+    readStringByte(0);
+  }
+
+  const tempoValue = readInt();
+
   return {
     version: { major, minor },
     title,
@@ -70,7 +113,8 @@ module.exports = buf => {
     tab,
     instructions,
     comments,
-    lyrics: { track: lyricTrack, from: lyricFrom, text: lyricText }
+    lyrics: { track: lyricTrack, from: lyricFrom, text: lyricText },
+    tempoValue
   };
 };
 
